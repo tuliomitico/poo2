dae2be99610c05435031db95b745319f07005d72
Túlio de Freitas Castro <tulioneedforspeed@gmail.com>
2023-03-03 20:13:41 -0300

    adiciona primeiros exercicios da lab

---
+++ Exercicio1.java
@@ -0,0 +1,10 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        String palavra = "Verdade";
+        String dois_chars = "de";
+        ComparaStrings comparador = new ComparaStrings();
+        FrequenciaDeLetras freq = new FrequenciaDeLetras();
+        System.out.println(comparador.compara_strings(palavra,dois_chars));
+        freq.conta_letras("Arara");
+    }
+}
